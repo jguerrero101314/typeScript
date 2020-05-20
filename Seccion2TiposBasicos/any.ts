54639c162748e29c2a7c9b1e6b429bea62cd1a78
jguerrero101314 <joelg1014@hotmail.com>
2020-05-20 10:45:58 -0500

    15. Tipo Any

---
+++ Seccion2TiposBasicos/any.ts
@@ -0,0 +1,11 @@
+let vengador;
+let existe;
+let derrotas;
+vengador = "Dr. Strange";
+console.log(vengador.charAt(0));
+vengador = 150.5555;
+console.log(vengador.toFixed(2));
+vengador=true;
+console.log(vengador);
+console.log(existe);
+console.log(derrotas);
---
+++ Seccion2TiposBasicos/strings.js
@@ -0,0 +1,9 @@
+var batman = "batman";
+var linternaVerde = "Linterna verde";
+var volcanNegro = "Volcan negro";
+console.log(batman);
+console.log(linternaVerde);
+console.log(volcanNegro);
+var concatenar = "Los h\u00E9roes: " + batman + "," + linternaVerde + ", " + volcanNegro;
+var concat = "Los héroes: " + batman + "," + linternaVerde + ", " + volcanNegro;
+console.log(concatenar);
