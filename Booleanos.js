4137457be61dfe39f2422f1fc19a2c60ddf5a4e3
jguerrero101314 <joelg1014@hotmail.com>
2020-05-20 10:26:40 -0500

    12. Booleans - Booleanos

---
+++ Booleanos.js
@@ -0,0 +1,14 @@
+
+var esSuperman = true;
+var esBatman;
+var esAcuaman = true;
+if (esSuperman) {
+    console.log("Estamos salvados!!!");
+}
+else {
+    console.log("Ooops! ohh");
+}
+esSuperman = convertirClark();
+function convertirClark() {
+    return false;
+}
---
+++ Booleanos.ts
@@ -0,0 +1,13 @@
+let esSuperman:boolean = true;
+let esBatman:boolean;
+let esAcuaman = true;
+
+if(esSuperman){
+  console.log("Estamos salvados!!!");
+}else{
+    console.log("Ooops! ohh");
+}
+esSuperman = convertirClark();
+function convertirClark(){
+  return false;
+}
