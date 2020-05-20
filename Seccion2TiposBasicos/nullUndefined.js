577fa4be1b200be2d0c303fbcf19a2e263a8929c
jguerrero101314 <joelg1014@hotmail.com>
2020-05-20 11:14:31 -0500

    22. Null y Undefined

---
+++ Seccion2TiposBasicos/nullUndefined.js
@@ -0,0 +1,8 @@
+"use strict";
+/*let nada:undefined = undefined;
+nada = null;*/
+var ironman;
+ironman = "tony";
+ironman = undefined;
+var numero = undefined;
+numero = 10;
---
+++ Seccion2TiposBasicos/nullUndefined.ts
@@ -0,0 +1,8 @@
+/*let nada:undefined = undefined;
+nada = null;*/
+let ironman:string;
+ironman ="tony";
+ironman = undefined;
+
+let numero:number = undefined;
+numero =10;
