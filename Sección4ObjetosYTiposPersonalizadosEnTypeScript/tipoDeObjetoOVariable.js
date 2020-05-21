d1b968aac0d41aceb372f1751f9fa075960154af
jguerrero101314 <joelg1014@hotmail.com>
2020-05-21 14:29:59 -0500

    41. Revisar el tipo de un objeto o variable

---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/tipoDeObjetoOVariable.js
@@ -0,0 +1,9 @@
+"use strict";
+var cosa = {};
+console.log(typeof cosa);
+if (typeof cosa === "number") {
+    console.log("Cosa es un numero");
+}
+else {
+    console.log("Esste codigo indica que no es un numero");
+}
---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/tipoDeObjetoOVariable.ts
@@ -0,0 +1,7 @@
+let cosa:any = {};
+ console.log(typeof cosa);
+if(typeof cosa === "number"){
+  console.log("Cosa es un numero");
+}else{
+  console.log("Esste codigo indica que no es un numero");
+}
