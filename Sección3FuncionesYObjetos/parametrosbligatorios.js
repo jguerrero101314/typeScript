dae3507b20207389b31faac35376ad07e00c4505
jguerrero101314 <joelg1014@hotmail.com>
2020-05-21 12:09:57 -0500

    27. Parámetros obligatorios de las funciones

---
+++ Sección3FuncionesYObjetos/parametrosbligatorios.js
@@ -0,0 +1,6 @@
+"use strict";
+function nombreCompleto(nombre, apellido) {
+    return nombre + ' ' + apellido;
+}
+var nombre = nombreCompleto('Joel', 'Guerrero');
+console.log(nombre);
---
+++ Sección3FuncionesYObjetos/parametrosbligatorios.ts
@@ -0,0 +1,7 @@
+function nombreCompleto(nombre:string, apellido:string):string{
+
+  return nombre + ' ' + apellido;
+}
+
+let nombre = nombreCompleto('Joel','Guerrero');
+console.log(nombre);
