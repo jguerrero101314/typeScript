e3d6ae50e25fb768bbbabd88b9b69f8c45b74887
jguerrero101314 <joelg1014@hotmail.com>
2020-05-21 12:06:36 -0500

    26. Funciones básicas

---
+++ Sección3FuncionesYObjetos/FuncionesBasicas.js
@@ -0,0 +1,10 @@
+"use strict";
+var heroe = "Flash";
+function imprime_heroe() {
+    return heroe;
+}
+var activar_batisenal = function () {
+    return "Batisenal activada.";
+};
+console.log(imprime_heroe());
+console.log(activar_batisenal());
---
+++ Sección3FuncionesYObjetos/FuncionesBasicas.ts
@@ -0,0 +1,10 @@
+let heroe:string = "Flash";
+
+function imprime_heroe():string{
+  return heroe;
+}
+let activar_batisenal = function():string{
+  return "Batisenal activada.";
+}
+console.log(imprime_heroe());
+console.log(activar_batisenal());
