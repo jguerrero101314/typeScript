ff597c01beef3e6edf02fd14c940d4437df0b821
jguerrero101314 <joelg1014@hotmail.com>
2020-05-21 14:11:59 -0500

    36. ¿Cómo crear objetos con tipos específicos?

---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/objetosConTiposEspecificos.js
@@ -0,0 +1,9 @@
+"use strict";
+var flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Puede correr muy rapido", "viajar por el tiempo"],
+    getNombre: function () {
+    }
+};
+flash.getNombre();
---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/objetosConTiposEspecificos.ts
@@ -0,0 +1,10 @@
+let flash:{nombre:string, edad:number, poderes:String[]} = {
+  nombre:"Barry Allen",
+  edad:24,
+  poderes:["Puede correr muy rapido", "viajar por el tiempo"],
+  getNombre(){
+
+  }
+};
+
+flash.getNombre();
