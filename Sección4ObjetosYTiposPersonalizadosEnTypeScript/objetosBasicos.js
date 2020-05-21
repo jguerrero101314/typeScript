78c3516af76fe0a5d527b7e45a698b73650e456e
jguerrero101314 <joelg1014@hotmail.com>
2020-05-21 14:08:02 -0500

    35. Objetos básicos

---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/objetosBasicos.js
@@ -0,0 +1,11 @@
+"use strict";
+var flash = {
+    nombre: "Barry Allen",
+    edad: 24,
+    poderes: ["Puede correr muy rapido", "viajar por el tiempo"]
+};
+flash = {
+    nombre: "Clark Kent",
+    edad: 500,
+    poderes: ["poder volar"]
+};
---
+++ Sección4ObjetosYTiposPersonalizadosEnTypeScript/objetosBasicos.ts
@@ -0,0 +1,10 @@
+let flash = {
+  nombre:"Barry Allen",
+  edad:24,
+  poderes:["Puede correr muy rapido", "viajar por el tiempo"]
+};
+flash = {
+  nombre: "Clark Kent",
+  edad: 500,
+  poderes:["poder volar"]
+}
