f786892edeabd14784375cb8e242475508ff2453
jguerrero101314 <joelg1014@hotmail.com>
2020-05-22 12:37:07 -0500

    59. Destructuración de Objetos

---
+++ Seccion6/destructuracion.js
@@ -0,0 +1,7 @@
+var avengers1 = {
+    nick: "Samuel Jackson",
+    ironman1: "Robert Downey Jr",
+    vision: "Paul Bettany"
+};
+var nick = avengers1.nick, ironman1 = avengers1.ironman1, vision = avengers1.vision, _a = avengers1.thor, thor = _a === void 0 ? "Paulino Callejas" : _a;
+console.log(nick + " \n" + ironman1 + " \n" + vision);
---
+++ Seccion6/destructuracion.ts
@@ -0,0 +1,7 @@
+let avengers1 = {
+    nick: "Samuel Jackson",
+    ironman1: "Robert Downey Jr",
+    vision: "Paul Bettany"
+}
+let { nick, ironman1, vision, thor="Paulino Callejas" } = avengers1;
+console.log(nick + " \n" + ironman1 + " \n" + vision);
