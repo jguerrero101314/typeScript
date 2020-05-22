83fba6c92f6c28fea9748f957a8d9504b3819a57
jguerrero101314 <joelg1014@hotmail.com>
2020-05-22 10:52:53 -0500

    47. ¿Es posible la depuración del código de TypeScript?

--- Sección5DepuracióndeErroresyelarchivotsconfigJson/app.js
+++ Sección5DepuracióndeErroresyelarchivotsconfigJson/app.js
@@ -1,3 +1,11 @@
 "use strict";
 var edad = 30;
-var prueba = undefined;
+var heroe = "Ricardo Tapia (Robin)";
+var mensaje = imprimir(heroe, edad);
+console.log(mensaje);
+function imprimir(heroe, edad) {
+    heroe = heroe.toLowerCase();
+    edad = edad + 10;
+    return heroe + " " + edad;
+}
+//# sourceMappingURL=app.js.map
--- Sección5DepuracióndeErroresyelarchivotsconfigJson/app.ts
+++ Sección5DepuracióndeErroresyelarchivotsconfigJson/app.ts
@@ -1,2 +1,11 @@
 let edad:number = 30;
-let prueba:string = undefined;
+let heroe:string = "Ricardo Tapia (Robin)";
+
+let mensaje = imprimir(heroe,edad);
+console.log(mensaje);
+function imprimir(heroe:string,edad:number):string{
+  heroe = heroe.toLowerCase();
+  edad = edad + 10;
+
+  return heroe + " " + edad;
+}
