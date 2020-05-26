a38a5333487a0a3d6bec14f0eeb45956b3ace43f
jguerrero101314 <joelg1014@hotmail.com>
2020-05-26 12:14:59 -0500

    funcionesGenericas.js

---
+++ Seccion11Genericos/funcionesGenericas.js
@@ -0,0 +1,7 @@
+"use strict";
+function regresar(arg) {
+    return arg;
+}
+console.log(regresar(15.456789).toFixed(2));
+console.log(regresar("Ricardo Tapia").toLowerCase());
+console.log(regresar(new Date().getDay()));
