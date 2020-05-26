8b7c2ddd68896c272d1beb43e2d6ac484bc63133
jguerrero101314 <joelg1014@hotmail.com>
2020-05-26 12:27:15 -0500

    104. Arreglos Genéricos

---
+++ Seccion11Genericos/arreglosGenericos.js
@@ -0,0 +1,4 @@
+"use strict";
+var heroes2 = ["Flash", "Batman", "Superman"];
+heroes.push("123");
+var villanos3 = ["Lex Luthor", "Flash Reverso"];
---
+++ Seccion11Genericos/arreglosGenericos.ts
@@ -0,0 +1,4 @@
+let heroes2: Array<string> = ["Flash","Batman","Superman"];
+heroes.push("123");
+
+let villanos3:string[] = ["Lex Luthor","Flash Reverso"];  
