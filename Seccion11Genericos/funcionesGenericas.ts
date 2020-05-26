6216c8680bbb7292a4729b07a001a581b8dc149d
jguerrero101314 <joelg1014@hotmail.com>
2020-05-26 12:14:52 -0500

    102. Creando funciones genéricas

---
+++ Seccion11Genericos/funcionesGenericas.ts
@@ -0,0 +1,6 @@
+function regresar<T>(arg:T){
+  return arg;
+}
+console.log(regresar( 15.456789 ).toFixed(2));
+console.log(regresar( "Ricardo Tapia").toLowerCase());
+console.log(regresar( new Date().getDay()));
